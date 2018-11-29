54765fc6f11456a18c07772e2af2d9c0517b4b9a
unknown <351426081@qq.com>
2018-11-29 16:22:16 +0800

    nothing

--- testCase/123.py
+++ testCase/123.py
@@ -1,10 +1,20 @@
-class printhanshu():
 
-    def printa(self,i):
-        print(i)
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common import action_chains
+from selenium.webdriver.support.ui import WebDriverWait
+from time import sleep
+from selenium.webdriver.common.by import By
+driver  = webdriver.Chrome()
 
-    def diaoyong(self,i):
-        for b in range(i):
-            return printhanshu.printa(self,b)
-
-printhanshu().diaoyong(5)
+driver.get("https://pre.svocloud.com")
+a = driver.get_cookies()
+print(a)
+driver.find_element_by_css_selector("[formcontrolname='username']").send_keys(18600929870)
+driver.find_element_by_css_selector("[formcontrolname='password']").send_keys(123456)
+driver.find_element_by_css_selector("button").click()
+#提交操作；submit可以提交内容，来达到clike的作用
+#driver.find_element_by_css_selector("a[herf='#/page/meeting/conference-management']").submit()
+sleep(5)
+a = driver.find_element_by_css_selector("a > span").text
+print(a)
