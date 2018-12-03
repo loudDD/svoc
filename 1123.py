015a5bfdfcdddbe7eeddcc289ebe01e75a44c15e
unknown <351426081@qq.com>
2018-12-03 16:22:16 +0800

    add md,bash,vim

--- 1123.py
+++ 1123.py
@@ -1,1 +1,20 @@
-print("123123")
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Firefox()
+
+driver.get("https://www.baidu.com")
+
+try:
+    a = driver.find_element_by_id("kw")
+    print()
+    print(a.get_attribute())
+finally:
+    driver.quit()
+
+# ele = WebDriverWait(driver,10,0.5).until(EC.visibility_of_element_located(driver.find_element_by_css_selector("")))
+# driver.implicitly_wait(10)
+
+
