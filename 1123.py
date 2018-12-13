01ee6fa07327f02643e3a3cbe4fdc3d5e6b13ad8
unknown <351426081@qq.com>
2018-12-13 09:24:39 +0800

    nothing again

--- 1123.py
+++ 1123.py
@@ -1,20 +1,7 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
 
+def add(x,y):
+    return x + y
 
-driver = webdriver.Firefox()
-
-driver.get("https://www.baidu.com")
-
-try:
-    a = driver.find_element_by_id("kw")
-    print()
-    print(a.get_attribute())
-finally:
-    driver.quit()
-
-# ele = WebDriverWait(driver,10,0.5).until(EC.visibility_of_element_located(driver.find_element_by_css_selector("")))
-# driver.implicitly_wait(10)
-
-
+num = [1,2,3,4]
+b = list(map((lambda x,y: x + y),(num),[1,2,3,4]))
+print(b)
